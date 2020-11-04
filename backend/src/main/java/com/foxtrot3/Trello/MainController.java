b0ecae457069dcb8e2f0102f949f01b6348e9d3f
Zientak.David <Zenakz@github.com>
2020-11-04 15:31:10 +0100

    added database connection to helloWorld

--- backend/src/main/java/com/foxtrot3/Trello/MainController.java
+++ backend/src/main/java/com/foxtrot3/Trello/MainController.java
@@ -1,13 +1,17 @@
 package com.foxtrot3.Trello;
 
+import com.foxtrot3.Trello.Database.HelloRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    @Autowired
+    HelloRepo helloRepo;
 
     @GetMapping("/hello")
     String helloWorld(){
-        return "Hello World!";
+        return helloRepo.findAll().get(0).getHello();
     }
 }
