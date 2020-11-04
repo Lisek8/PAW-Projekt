1f62cc0dbb3cbeb9f11854cefe3d3d7b9dbc9cd9
215848 <215848@edu.p.lodz.pl>
2020-11-04 18:43:30 +0100

    Menu bar

---
+++ frontend/src/components/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <img src="../assets/trelloIcon.png" alt="Trello">
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Menu'
+}
+</script>
+
+<style scoped>
+    .header {
+    background-color: #3467eb;
+    text-align: center;
+    padding: 10px;
+    }
+    .header a{
+        padding-right: 5px;
+    }
+</style>
