8d57368c4af1de36d0af134257eb08017873c90b
215848 <215848@edu.p.lodz.pl>
2020-11-04 17:59:03 +0100

    hello world request

---
+++ frontend/src/components/Content.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>test</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Content',
+  created () {
+    axios.get('http://localhost:8080/hello')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err))
+  }
+}
+</script>
+
+<style scoped>
+</style>
