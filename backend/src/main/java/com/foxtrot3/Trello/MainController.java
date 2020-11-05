14c1df8564c95f34afb21cee78393d05c22c2b60
Zientak.David <Zenakz@github.com>
2020-11-05 15:07:42 +0100

    added rest-services path

--- backend/src/main/java/com/foxtrot3/Trello/MainController.java
+++ backend/src/main/java/com/foxtrot3/Trello/MainController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@RequestMapping(value = "/rest-services")
 public class MainController {
     @Autowired
     HelloRepo helloRepo;
