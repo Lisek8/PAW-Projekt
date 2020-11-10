92961655ac9173401eb7e36671049b76fabd4d8a
Zientak.David <Zenakz@github.com>
2020-11-10 23:29:11 +0100

    added user verification endpoints

--- backend/src/main/java/com/foxtrot3/trello/MainController.java
+++ backend/src/main/java/com/foxtrot3/trello/MainController.java
@@ -1,13 +1,29 @@
 package com.foxtrot3.trello;
 
 import com.foxtrot3.trello.database.HelloRepo;
+import com.foxtrot3.trello.database.User;
+import com.foxtrot3.trello.database.UserRepo;
+import com.foxtrot3.trello.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Optional;
 
 
 @Controller
@@ -16,10 +32,31 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController extends SpringBootServletInitializer {
     @Autowired
     HelloRepo helloRepo;
+    @Autowired
+    UserRepo userRepo;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
 
     @GetMapping("/hello")
     @ResponseBody
     public String helloWorld(){
         return helloRepo.findAll().get(0).getHello();
     }
+
+    @PostMapping("/register")
+    void registerUser(String email, String password, String name){
+        if(userRepo.findByEmail(email).isPresent()){
+            throw new RuntimeException("User with that email already exists");
+        }
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setName(name);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setUserType("USER");
+        userRepo.save(user);
+    }
+
+
 }
---
+++ backend/src/main/java/com/foxtrot3/trello/security/ApplicationUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.foxtrot3.trello.security;
+
+import com.foxtrot3.trello.database.User;
+import com.foxtrot3.trello.database.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class ApplicationUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email).orElseThrow(()->new RuntimeException("User 404"));
+        return new UserPrincipal(user);
+    }
+
+}
