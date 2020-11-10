f16d7bbabd0ecef9e95a5a16ef91b34e36cd9e07
Zientak.David <Zenakz@github.com>
2020-11-10 23:38:55 +0100

    changed register parameters to a requestbody

--- backend/src/main/java/com/foxtrot3/trello/MainController.java
+++ backend/src/main/java/com/foxtrot3/trello/MainController.java
@@ -1,12 +1,14 @@
 package com.foxtrot3.trello;
 
 import com.foxtrot3.trello.database.HelloRepo;
+import com.foxtrot3.trello.database.RegisterForm;
 import com.foxtrot3.trello.database.User;
 import com.foxtrot3.trello.database.UserRepo;
 import com.foxtrot3.trello.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,10 +18,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -44,15 +43,16 @@ public class MainController extends SpringBootServletInitializer {
         return helloRepo.findAll().get(0).getHello();
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/register")
-    void registerUser(String email, String password, String name){
-        if(userRepo.findByEmail(email).isPresent()){
+    void registerUser(@RequestBody RegisterForm registerForm){
+        if(userRepo.findByEmail(registerForm.getEmail()).isPresent()){
             throw new RuntimeException("User with that email already exists");
         }
         User user = new User();
-        user.setEmail(email);
-        user.setPassword(password);
-        user.setName(name);
+        user.setEmail(registerForm.getEmail());
+        user.setPassword(registerForm.getPassword());
+        user.setName(registerForm.getName());
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setUserType("USER");
         userRepo.save(user);
