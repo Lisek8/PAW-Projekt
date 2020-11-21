8908e99652a57abf5c40ef9bd25fc307623d1a32
Zientak.David <Zenakz@github.com>
2020-11-21 10:49:17 +0100

    heroku fix test

--- backend/src/main/java/com/foxtrot3/trello/security/WebConfig.java
+++ backend/src/main/java/com/foxtrot3/trello/security/WebConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+/*
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
@@ -14,3 +14,4 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**");
     }
 }
+*/
