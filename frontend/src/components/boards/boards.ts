2508e6dc72c745116c18ac8af08a32f7c4826780
Lisek8 <fafik989@gmail.com>
2020-12-06 01:12:28 +0100

    Reduced image resolution

--- frontend/src/components/boards/boards.ts
+++ frontend/src/components/boards/boards.ts
@@ -19,52 +19,52 @@ export default class Boards extends Vue {
     this.recentlyViewed.push(
       {
         title: 'Test0',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test0'
       },
       {
         title: 'Test1',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test1'
       },
       {
         title: 'Test2',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test2'
       },
       {
         title: 'Test3',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test3'
       },
       {
         title: 'Test4',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test4'
       },
       {
         title: 'Test5',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test5'
       },
       {
         title: 'Test6',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test6'
       },
       {
         title: 'Test7',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test7'
       },
       {
         title: 'Test8',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test8'
       },
       {
         title: 'Test9',
-        image: Environment.publicPath + 'assets/basic.png',
+        image: Environment.publicPath + 'assets/basicPreview.png',
         id: 'Test9'
       }
     );
@@ -81,7 +81,7 @@ export default class Boards extends Vue {
         for (let i = 0; i < res.data.length; i++) {
           const board = {
             title: res.data[i].name,
-            image: Environment.publicPath + 'assets/basic.png',
+            image: Environment.publicPath + 'assets/basicPreview.png',
             id: res.data[i].id
           };
           this.privateBoards.push(board);
