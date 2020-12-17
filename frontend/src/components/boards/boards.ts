462e837e5bddd3a5ad471c795c53f750b4788d61
Lisek8 <fafik989@gmail.com>
2020-12-17 17:01:18 +0100

    Removed settings and recently viewed

--- frontend/src/components/boards/boards.ts
+++ frontend/src/components/boards/boards.ts
@@ -11,63 +11,9 @@ import { ProvideReactive } from 'vue-property-decorator';
   }
 })
 export default class Boards extends Vue {
-  public recentlyViewed: Board[] = [];
   @ProvideReactive() privateBoards: Board[] = [];
 
   mounted () {
-    // Get them from backend
-    this.recentlyViewed.push(
-      {
-        title: 'Test0',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test0'
-      },
-      {
-        title: 'Test1',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test1'
-      },
-      {
-        title: 'Test2',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test2'
-      },
-      {
-        title: 'Test3',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test3'
-      },
-      {
-        title: 'Test4',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test4'
-      },
-      {
-        title: 'Test5',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test5'
-      },
-      {
-        title: 'Test6',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test6'
-      },
-      {
-        title: 'Test7',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test7'
-      },
-      {
-        title: 'Test8',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test8'
-      },
-      {
-        title: 'Test9',
-        image: Environment.publicPath + 'assets/basicPreview.png',
-        id: 'Test9'
-      }
-    );
     const config = {
       headers: {
         Authorization: 'Bearer ' + localStorage.getItem('jwt')
